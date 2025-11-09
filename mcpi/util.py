085b1af766ec093222685195901142b839bbdded
Misha Gusev @haxgun <misha@valory.su>
2025-11-09 21:20:48 +0500

    feat: update encode to utf-8 instead CP437

--- mcpi/util.py
+++ mcpi/util.py
@@ -1,18 +1,23 @@
 import collections
 
+
 def flatten(l):
     for e in l:
         if isinstance(e, collections.abc.Iterable) and not isinstance(e, str):
-            for ee in flatten(e): yield ee
-        else: yield e
+            for ee in flatten(e):
+                yield ee
+        else:
+            yield e
+
 
 def flatten_parameters_to_bytestring(l):
     return b",".join(map(_misc_to_bytes, flatten(l)))
 
+
 def _misc_to_bytes(m):
     """
-    Convert an arbitrary object into a string encoded as a CP437 series of bytes.
+    Convert an arbitrary object into a string encoded as a utf-8 series of bytes.
 
     See `Connection.send` for more details.
     """
-    return str(m).encode("cp437")
+    return str(m).encode("utf-8")
